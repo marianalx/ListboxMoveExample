2a50c80e16aa46ac77515f5a7697fd863b9ed1d3
MW <marian.witkowski@gmail.com>
2020-04-22 21:45:14 +0200

    initial commit

---
+++ ListboxMoveExample/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListboxMoveExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <=20; i++)
+            {
+                listBox1.Items.Add(String.Format("Opcja {0}", i));
+                listBox2.Items.Add(String.Format("Opcja {0}", (char)(i + (int)'A')) );
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //listBox1.SelectedIndices
+        }
+    }
+}
